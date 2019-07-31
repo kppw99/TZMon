ba4a115b197937ba494e7f39b88817fdc7655e22
kevin <kppw99@gmail.com>
2019-07-31 15:33:01 +0900

    delete: 2048 game source
    Signed-off-by: kevin <kppw99@gmail.com>
    Change-Id: I129dfdfbaa77c0d1d3f662cbeb04385ca0611315

--- mobileGame/2048-android/src/main/jni/tzmonSocket.h
+++
@@ -1,7 +0,0 @@
-#ifndef BLOCKINGER_TZMONSOCKET_H
-#define BLOCKINGER_TZMONSOCKET_H
-
-bool _call_tzmonTA(char *cmd, char *out, int *outLen);
-bool _call_tzmonAbusingDetection(const char *appName);
-
-#endif //BLOCKINGER_TZMONSOCKET_H
